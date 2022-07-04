b8cee6dba4b9591292a16ff2d22396ff4a2f6ae8
iamsid <developer.sid@outlook.com>
2022-07-04 18:25:07 +0300

    add HTML markup and CSS styles

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import _ from 'lodash';
 import './styles.css';
 
 function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+  // const element = document.createElement('div');
+  // // Lodash, now imported by this script
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
