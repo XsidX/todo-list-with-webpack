4e0117cd8fb93d8ebf9d0fdec4e33ede174ff887
iamsid <developer.sid@outlook.com>
2022-07-04 21:28:04 +0300

    dynamically add todo list from a placeholder todo array

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
-import _ from 'lodash';
 import './styles.css';
 
-function component() {
-  // const element = document.createElement('div');
-  // // Lodash, now imported by this script
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // return element;
-}
+const todo = [
+  {
+    description: 'onelsdvsdvsdvsdvs',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'threelsdvsdvsdvsdvs',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'twolsdvsdvsdvsdvs',
+    completed: true,
+    index: 2,
+  },
+];
 
-// document.body.appendChild(component());
+const sortedTodo = todo.sort((prev, next) => prev.index - next.index);
+
+const form = document.getElementById('form');
+sortedTodo.forEach(({ description }) => {
+  const listItem = document.createElement('div');
+  listItem.className = 'row list-item';
+  listItem.innerHTML = `
+    <svg class="icon-check">
+      <use
+        xlink:href="/web/20180320194056mp_/http://www.getminimalist.com/assets/icons.svg#icon-check"
+      ></use>
+    </svg>
+    <input class="todo-item" type="text" value=${description} />
+    <svg
+      width="25px"
+      height="30px"
+      class="icon-move"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path
+        d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
+        fill="#b6b6b8"
+      ></path>
+    </svg>
+  `;
+
+  form.appendChild(listItem);
+});
