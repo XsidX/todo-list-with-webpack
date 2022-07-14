5788f699aac74d19afdcc95fdbc2722ba578ca35
FADAHUNSI <oluwaseyisamuel018@gmail.com>
2022-07-14 13:38:57 +0100

    Add test for status change and clear completed status

--- src/status.test.js
+++ src/status.test.js
@@ -1,4 +1,6 @@
 import { Store, editTodos } from './crudOps.js';
+import { updateStatus, clearCompleted } from './status.js';
+
 
 describe('editTodos()', () => {
   beforeEach(() => {
@@ -26,5 +28,22 @@ describe('local storage status', () => {
 
   beforeEach(() => {
     localStorage.setItem('todos', JSON.stringify([]));
+  });
+
+  it('should update the completed status of a todo', () => {
+    const index = 0;
+    const todo = { description: 'test', completed: false, index };
+    Store.addTodo(todo);
+    updateStatus(index);
+    expect(JSON.parse(localStorage.getItem('todos'))[0].completed).toEqual(true);
+  });
+
+  it('should clear all completed todos', () => {
+    const todo = { description: 'test', completed: false, index: 0 };
+    const todo2 = { description: 'test2', completed: true, index: 1 };
+    Store.addTodo(todo);
+    Store.addTodo(todo2);
+    clearCompleted();
+    expect(JSON.parse(localStorage.getItem('todos'))).toEqual([todo]);
   });
 });
